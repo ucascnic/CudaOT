314fc21c15e8b138ec0a4904eb3015e34d7d88ac
CudaOT <55936546+ucascnic@users.noreply.github.com>
2022-11-10 21:12:24 +0800

    update code

--- Color transfer between images/SparseSinkhorn.py
+++ Color transfer between images/SparseSinkhorn.py
@@ -19,6 +19,156 @@ from pykeops.torch import generic_argmin
 from pykeops.torch import generic_sum,generic_logsumexp,generic_argmin
 use_cuda = torch.cuda.is_available()
 dtype = torch.cuda.DoubleTensor if use_cuda else torch.FloatTensor
+
+
+import numpy as np
+import torch
+from random import choices
+
+from pykeops.torch import LazyTensor
+from pykeops.torch.cluster import grid_cluster
+import pandas as pd
+from pykeops.torch.cluster import cluster_ranges_centroids
+from pykeops.torch import generic_argmin
+from pykeops.torch import generic_sum,generic_logsumexp,generic_argmin
+use_cuda = torch.cuda.is_available()
+dtype = torch.cuda.DoubleTensor if use_cuda else torch.FloatTensor
+
+
+def sinkhorn_on_log_domain_GPU_memory_optimize(a, b, M, reg, epsilon0 = 1e6, numItermax=10,stopThr=1e-9):
+    
+    
+    n = len(a)
+    m = len(b)
+    device = torch.device('cuda')
+    reg = torch.as_tensor(reg, dtype=torch.float64,device=device)
+    a = torch.as_tensor(a, dtype=torch.float64,device=device)
+    b = torch.as_tensor(b, dtype=torch.float64,device=device)
+    a = torch.reshape(a,(n,1))
+    b = torch.reshape(b,(1,m))
+    regk = torch.as_tensor(epsilon0, dtype=torch.float64,device=device)
+ 
+    loop = 1
+    cpt = torch.as_tensor(1, dtype=torch.float64,device=device)
+    cptt = 0
+    err = 1
+    
+    f = torch.zeros_like(a , dtype=torch.float64,device=device)
+    g = torch.zeros_like(b , dtype=torch.float64,device=device)
+    
+    def get_reg(n):  # exponential decreasing
+        return (epsilon0 - reg) * torch.exp(-n) + reg
+    
+    def get_K(alpha, beta,reg):
+        """log space computation"""
+        M_ = torch.clone(M)  
+        M_ -= alpha
+        M_ -= beta
+        M_ /= -reg
+        M_ = M_.exp()
+        res = M_.sum(1,keepdim = True)
+        M_ = torch.detach(M_)
+        M_ = M_.cpu()
+        del M_      
+        res = torch.detach(res)
+        return res
+ 
+
+    subset = (M < 0);
+    numIterinner = [5,10,10,50,50,200]
+    nn = len(numIterinner) - 1
+    while loop:
+         
+
+            
+        regk = torch.max(regk*0.1,reg)
+        regk = get_reg(cpt)
+        ind = [nn,cptt][cptt<=nn]
+        
+        for i in range(numIterinner[ind]):
+            #print(torch.cuda.memory_allocated())
+            torch.cuda.empty_cache()
+            M_ = torch.clone(M)  
+            M_ -=  g
+
+
+            min_f,_ = torch.min(M_, dim = 1,  keepdim = True)
+            
+            M_ -= min_f
+            M_ /= -regk
+
+            M2 = torch.exp(M_)
+            M2[subset] = 0
+            temp = M2.sum(1,keepdim = True)
+
+            M2 = M2.cpu()
+            del M2
+            M_ = M_.cpu()
+            del M_
+            temp_ = torch.clone(temp)
+            temp_ = torch.detach(temp_)
+            temp = temp.cpu()
+            del temp
+
+            torch.cuda.empty_cache()
+
+            f = regk * torch.log(a) -\
+            regk * torch.log(temp_) + min_f
+
+            
+            M_ = torch.clone(M)  
+            M_ -=  f
+
+            min_g,_ = torch.min(M_, dim = 0, keepdim = True)
+            M_ -= min_g
+            M_ /= -regk 
+            M2 = torch.exp(M_)
+            M2[subset] = 0
+            temp = M2.sum(0,keepdim = True)
+
+            #print("clear")
+            #print(torch.cuda.memory_allocated())
+            M2 = M2.cpu()
+
+            del M2
+            M_ = M_.cpu()
+            if cptt == 1:
+                subset = (M_ < torch.log(torch.tensor([1e-10])))
+            del M_
+            temp_ = torch.clone(temp)
+            temp_ = torch.detach(temp_)
+            temp = temp.cpu()
+            del temp            
+            #print(torch.cuda.memory_allocated())
+            g = regk * torch.log(b) -\
+            regk * torch.log(temp_) + min_g
+ 
+        #print(b.shape)
+        #break      
+        if cpt % 1 == 0:
+ 
+            res = get_K(f,g,regk)
+            print(res.shape)
+            print(a.shape)
+            res -= a
+            err2 =  torch.norm(res ,2) **2
+            print(err2)
+            #break
+            #return res,res,res
+            pass
+        if err <= stopThr and cpt >= 50:
+
+            loop = False
+
+        if cpt >= numItermax:
+            loop = False 
+        print(cpt)
+        cpt += 1
+        cptt += 1
+    
+    #res = get_K(f,g,regk)
+    return f,g,regk
+
 def sinkhorn_on_log_domain_GPU_keops(mu, nv, x,y,  reg, epsilon0 = 1e6, numItermax=10,stopThr=1e-9,debug=0,ranges = None,
                                     warmf=None,warmg=None):
     
@@ -142,6 +292,203 @@ def sinkhorn_on_log_domain_GPU_keops(mu, nv, x,y,  reg, epsilon0 = 1e6, numIterm
         'x = Vi(3)',              # 1st input: dim-1 vector per line
 
         'y = Vj(3)',
+        
+        'f = Vi(1)', 
+        
+        'g = Vj(1)',
+        
+        'eps = Pm(1)',dtype  = 'float64' ) 
+    numIterinner = [5,10,10,50,50,100]
+    nn = len(numIterinner) - 1
+    
+ 
+    while loop:
+ 
+        
+        regk = torch.max(regk*0.1,reg)
+        regk = get_reg(cpt)
+
+        regk1 = one/regk
+        ind = [nn,cptt][cptt<=nn]
+ 
+        for i in range(numIterinner[ind]): 
+             
+            a_ = nearest_neighbor(x,y,g, ranges = ranges )
+            temp = x - y[ a_.view(-1).long()]
+            #print(x.shape)
+            #print(y.shape)
+            #print(f.shape)
+            #print(temp.shape)
+            min_f = torch.multiply(temp,temp).sum(dim=1, keepdim = True)  - g[ a_.view(-1).long()]
+            a = log_likelihood2(x,y,g,min_f,regk1, ranges = ranges )
+ 
+            f = regk * torch.log(mu) - regk * a  + min_f
+            #f = f*0
+            b_ = nearest_neighbor2(x,y,f, ranges = ranges ) 
+            
+            temp_ = x[ b_.view(-1).long()] -  y
+            min_g = (temp_ ** 2).sum(1, keepdim = True) - f[ b_.view(-1).long()]
+            #min_g = torch.multiply(temp,temp).sum(dim=0, keepdim = True) 
+            #min_g = min_g.T
+            #return  f,min_g,regk,temp_
+            b = log_likelihood_trans2(x,y,f,min_g, regk1, ranges = ranges )   
+ 
+            g = regk * torch.log(nv) - regk * b + min_g
+ 
+        #if debug:
+            #print(regk * torch.log(nv)- regk * b)
+            #break
+        if cpt % 10 == 0:
+            
+            res = torch.exp(sums(x,y,f,g,regk1, ranges = ranges )) 
+            res2 = torch.exp(sumsy(x,y,f,g,regk1, ranges = ranges )) 
+            print(torch.norm(res - mu)+torch.norm(res2 - nv))
+            if debug:
+                #print(res)
+                print(sum(res))
+                print(sum(res2))
+                #print(torch.norm(res - mu))
+                #print(torch.norm(res2 - nv))
+                break 
+            
+    
+            pass
+        if err <= stopThr and cpt >= 50:
+
+            loop = False
+
+        if cpt >= numItermax:
+            loop = False 
+        #print(cpt)
+        cpt += 1
+        cptt += 1
+
+  
+    return  f,g,regk
+
+def sinkhorn_on_log_domain_GPU_keops_high_dimension(d, mu, nv, x,y,  reg, epsilon0 = 1e6, numItermax=10,stopThr=1e-9,debug=0,ranges = None,
+                                    warmf=None,warmg=None):
+    
+    
+
+    device = torch.device('cuda')
+    reg = torch.as_tensor(reg, device=device).type(dtype)
+
+
+    regk = torch.as_tensor(epsilon0,  device=device).type(dtype)
+ 
+    loop = 1
+    cpt = torch.as_tensor([1],  device=device).type(dtype)
+    
+    one = torch.as_tensor([1.0],  device=device).type(dtype)
+    cptt = 0
+    err = 1
+    if warmf is not None:
+        f = torch.as_tensor(warmf  ).type(dtype)
+        g = torch.as_tensor(warmg ).type(dtype)
+    else:
+        
+        f = torch.zeros_like(mu ,device=device).type(dtype)
+        g = torch.zeros_like(nv ,device=device).type(dtype)
+    
+ 
+    #if debug:
+    #    print(f)
+    #    return f,f,f
+    def get_reg(n):  # exponential decreasing
+        return  (epsilon0 - reg) * torch.exp(-n) + reg 
+    
+    
+    nearest_neighbor = generic_argmin(
+    'SqNorm2(x-y)-g',   # Formula
+    'a = Vi(1)',      # Output: 1 scalar per line
+    'x = Vi('+str(d)+')',    
+    'y = Vj('+str(d)+')',
+    'g = Vj(1)',dtype  = 'float64'  )   
+    
+    nearest_neighbor2 = generic_argmin(
+    'SqNorm2(x-y)-f',   # Formula
+    'a = Vj(1)',      # Output: 1 scalar per line
+    'x = Vi('+str(d)+')',    
+    'y = Vj('+str(d)+')',
+    'f = Vi(1)',dtype  = 'float64'  )    
+    
+    log_likelihood = generic_logsumexp(
+       '(-eps * (SqNorm2(x-y)-g))  ', # Formula
+        
+        'a = Vi(1)',              # Output: 1 scalar per line
+
+        'x = Vi('+str(d)+')',              # 1st input: dim-1 vector per line
+
+        'y = Vj('+str(d)+')', 
+        
+        'g = Vj(1)',
+        
+        'eps = Pm(1)',dtype  = 'float64'  )               
+
+    log_likelihood2 = generic_logsumexp(
+       '(-eps * (SqNorm2(x-y)-g-minf))  ', # Formula
+        
+        'a = Vi(1)',              # Output: 1 scalar per line
+
+        'x = Vi('+str(d)+')',              # 1st input: dim-1 vector per line
+
+        'y = Vj('+str(d)+')', 
+        
+        'g = Vj(1)',
+        
+        'minf = Vi(1)',
+        
+        'eps = Pm(1)',dtype  = 'float64'  )  
+        
+    log_likelihood_trans = generic_logsumexp(
+       '(-eps * (SqNorm2(x-y)-f))  ', # Formula
+        
+        'a = Vj(1)',              # Output: 1 scalar per line
+
+        'x = Vi('+str(d)+')',              # 1st input: dim-1 vector per line
+
+        'y = Vj('+str(d)+')', 
+        
+        'f = Vi(1)',
+        
+        'eps = Pm(1)',dtype  = 'float64'  )   
+    
+    log_likelihood_trans2 = generic_logsumexp(
+       '(-eps * (SqNorm2(x-y)-f-ming))  ', # Formula
+        
+        'a = Vj(1)',              # Output: 1 scalar per line
+
+        'x = Vi('+str(d)+')',              # 1st input: dim-1 vector per line
+
+        'y = Vj('+str(d)+')', 
+        
+        'f = Vi(1)',         
+        'ming = Vj(1)',
+        
+        'eps = Pm(1)',dtype  = 'float64'  )    
+    sums = generic_logsumexp(
+       ' ( (-eps * (SqNorm2(x-y)-f-g)))  ', # Formula
+        
+        'a = Vi(1)',              # Output: 1 scalar per line
+
+        'x = Vi('+str(d)+')',              # 1st input: dim-1 vector per line
+
+        'y = Vj('+str(d)+')', 
+        
+        'f = Vi(1)', 
+        
+        'g = Vj(1)',
+        
+        'eps = Pm(1)',dtype  = 'float64'  ) 
+    sumsy = generic_logsumexp(
+       ' ( (-eps * (SqNorm2(x-y)-f-g)))  ', # Formula
+        
+        'a = Vj(1)',              # Output: 1 scalar per line
+
+        'x = Vi('+str(d)+')',             # 1st input: dim-1 vector per line
+
+        'y = Vj('+str(d)+')', 
         
         'f = Vi(1)', 
         
